08b8cd680cfdb118729cecf5b3d8120e7db3f303
Tetiana Sobko <sobkotiana@gmail.com>
2024-02-28 12:43:43 +0200

    created topic list, edited topic button

--- app/src/main/java/com/example/coursework/composables/TopicList.kt
+++ app/src/main/java/com/example/coursework/composables/TopicList.kt
@@ -6,16 +6,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -24,33 +27,61 @@ fun TopicButton() {
     Button(
         onClick = { /*TODO*/ },
         shape = RoundedCornerShape(8.dp),
-        contentPadding = PaddingValues(17.dp, 10.dp, 12.dp, 10.dp)
+        contentPadding = PaddingValues(17.dp, 10.dp, 0.dp, 10.dp),
+        modifier = Modifier
+            .fillMaxWidth()
     ) {
-        Box(modifier = Modifier.fillMaxWidth()) {
-            Column(verticalArrangement = Arrangement.spacedBy(6.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(6.dp),
+                modifier = Modifier.weight(9f)
+            ) {
                 Text(text = "Topic name")
                 Row(
-                    horizontalArrangement = Arrangement.spacedBy(12.dp),
-                    verticalAlignment = Alignment.CenterVertically
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
-                    LinearProgressIndicator()
+                    LinearProgressIndicator(
+                        progress = .5f,
+                        modifier = Modifier
+                            .weight(1f)
+                            .clip(RoundedCornerShape(8.dp))
+                    )
                     Text(text = "50%")
                 }
             }
-            Icon(
-                Icons.Filled.KeyboardArrowRight,
-                contentDescription = "Arrow",
-                modifier = Modifier.align(
-                    Alignment.CenterEnd
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    Icons.Filled.KeyboardArrowRight,
+                    contentDescription = "Arrow",
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(
+                            Alignment.CenterVertically
+                        )
                 )
-            )
+            }
         }
-
     }
 }
 
-@Preview
+@Composable
+fun TopicList() {
+    LazyColumn(
+        contentPadding = PaddingValues(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(3) {
+            TopicButton()
+        }
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun TopicsPreview() {
-    TopicButton()
+    TopicList()
 }
